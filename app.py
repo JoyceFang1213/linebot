3587cd7695bffde87be1523ba008541817e0555a
Joyce <objdoctor891213a@gmail.com>
2021-12-09 16:27:27 +0800

    add 1a2b

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-import os
+# import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -17,6 +17,9 @@ config.read('config.ini')
 
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
+x = random.sample('1234567890', 4)
+user_id_dic = {}
 
 
 # 接收 LINE 的資訊
@@ -39,21 +42,48 @@ def callback():
 
 # 學你說話
 @handler.add(MessageEvent, message=TextMessage)
-def pretty_echo(event):
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+def handle_message(event):
+    global x
+    global user_id_dic
+    user_id = event.source.user_id
+    user_id_dic[user_id] = x
 
-        # Phoebe 愛唱歌
-        pretty_note = '♫♪♬'
-        pretty_text = ''
+    y = event.message.text
+    if not y.isdigit():
+        message = TextSendMessage(text="Wrong Input!")
+        line_bot_api.reply_message(event.reply_token, message)
+        return
+    if len(y) != 4:
+        message = TextSendMessage(text="Wrong Input!")
+        line_bot_api.reply_message(event.reply_token, message)
+        return
+    if len(y) != len(set(y)):
+        message = TextSendMessage(text="Wrong Input!")
+        line_bot_api.reply_message(event.reply_token, message)
+        return
 
-        for i in event.message.text:
-            pretty_text += i
-            pretty_text += random.choice(pretty_note)
+    a = 0
+    b = 0
+    for i in range(len(y)):
+        if y[i] in user_id_dic[user_id]:
+            if y[i] == user_id_dic[user_id][i]:
+                a += 1
+            else:
+                b += 1
+    if a == 4:
+        x = random.sample('1234567890', 4)
 
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=pretty_text)
-        )
+    if a == 4:
+        message = [TextSendMessage(text="%d A %d B\nCorrect!!" % (a, b)),
+                   TextSendMessage(text="You are excellent!!")]
+        line_bot_api.reply_message(event.reply_token, message)
+
+    else:
+        message = [TextSendMessage(text="%d A %d B" % (a, b)),
+                   TextSendMessage(text="Try again!!")]
+        line_bot_api.reply_message(event.reply_token, message)
+
+    user_id_dic[user_id] = x
 
 
 if __name__ == "__main__":
