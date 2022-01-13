0fc51aaf95110a0ddb8f3c7d6a86533d272f0a9a
Joyce <objdoctor891213a@gmail.com>
2022-01-13 14:15:44 +0800

    add 1a2b

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import configparser
+import random
 
 app = Flask(__name__)
+x = random.sample('1234567890', 4)
+user_ID_dic = {}
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -34,9 +37,48 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    global x
+    global user_ID_dic
+    user_ID = event.source.user_id
+    user_ID_dic[user_ID] = x
+    
+    y = event.message.text
+    
+    if (y.isdigit() == False):
+        message = TextSendMessage(text= "Wrong Input!")
+        line_bot_api.reply_message(event.reply_token, message)
+        return 
+    if (len(y) != 4):
+        message = TextSendMessage(text= "Wrong Input!")
+        line_bot_api.reply_message(event.reply_token, message)
+        return 
+    if (len(y) != len(set(y))):
+        message = TextSendMessage(text= "Wrong Input!")
+        line_bot_api.reply_message(event.reply_token, message)
+        return 
+    
+    a = 0
+    b = 0
+    for i in range (len(y)):
+        if(y[i] in user_ID_dic[user_ID]):
+            if(y[i] == user_ID_dic[user_ID][i]):
+                a += 1
+            else:
+                b += 1
+    if(a == 4):
+        x = random.sample('1234567890', 4)
+    
+    if (a == 4):
+        message = [TextSendMessage(text= "%d A %d B\nCorrect!!" % (a, b)), 
+                   TextSendMessage(text= "You are excellent!!")]
+        line_bot_api.reply_message(event.reply_token, message)
+
+    else:
+        message = [TextSendMessage(text= "%d A %d B" % (a, b)), 
+                   TextSendMessage(text= "Try again!!")]
+        line_bot_api.reply_message(event.reply_token, message)
+     
+    user_ID_dic[user_ID] = x
 
 
 if __name__ == "__main__":
