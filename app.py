5bdfb30c98ff904c9abea93dbdb9282fe01c8be2
JoyceFang1213 <61085352+JoyceFang1213@users.noreply.github.com>
2022-01-15 12:05:09 +0800

    edit app.py

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import configparser
 import random
+import json
+import os
 
 app = Flask(__name__)
-x = random.sample('1234567890', 4)
-user_ID_dic = {}
+
+if not os.path.isfile("answer.json"):
+    with open("answer.json", "w") as out_file:
+        json.dump(dict(), out_file, indent=4)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -37,10 +41,13 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    global x
-    global user_ID_dic
+    with open("answer.json", "r") as in_file:
+        user_dict = json.load(in_file)
     user_ID = event.source.user_id
-    user_ID_dic[user_ID] = x
+    print(user_dict)
+
+    if user_ID not in user_dict:
+        user_dict[user_ID] = random.sample('1234567890', 4)
     
     y = event.message.text
     
@@ -60,26 +67,26 @@ def handle_message(event):
     a = 0
     b = 0
     for i in range (len(y)):
-        if(y[i] in user_ID_dic[user_ID]):
-            if(y[i] == user_ID_dic[user_ID][i]):
+        if(y[i] in user_dict[user_ID]):
+            if(y[i] == user_dict[user_ID][i]):
                 a += 1
             else:
                 b += 1
-    if(a == 4):
-        x = random.sample('1234567890', 4)
+    if (a == 4):
+        user_dict[user_ID] = random.sample('1234567890', 4)
     
     if (a == 4):
         message = [TextSendMessage(text= "%d A %d B\nCorrect!!" % (a, b)), 
                    TextSendMessage(text= "You are excellent!!")]
         line_bot_api.reply_message(event.reply_token, message)
-
     else:
         message = [TextSendMessage(text= "%d A %d B" % (a, b)), 
                    TextSendMessage(text= "Try again!!")]
         line_bot_api.reply_message(event.reply_token, message)
-     
-    user_ID_dic[user_ID] = x
 
+    with open("answer.json", "w") as output:
+        json.dump(user_dict, output, indent=4)
+    
 
 if __name__ == "__main__":
     app.run()
